f83d6908564929c3de0f07b776ff67a0c86e7a68
Miguel Figuerola <miguelfiguerolajimenez@gmail.com>
2018-06-07 12:49:22 +0200

    [Register User API] Home data.

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
 
 const pages = {
   home: {
-    literals: [{ hello: 'hola' }],
-    form: [
-      {
-        type: 'select',
-        values: []
-      }
-    ]
+    literals: {
+      header: "SIGNUP",
+      title: "WHAT KIND OF USER ARE YOU?",
+      guest: "A HOTEL GUEST",
+      agency: "A TRAVEL AGENCY",
+      company: "A COMPANY",
+      persuasiveMessage: "Enjoy a 10% discount on your reservation just for signup",
+      loginLink: "Are you registered?"
+    },
+    form: {
+      type: "select",
+      values: ["guest", "agency", "company"]
+    }
   }
 };
 
-app.route('/page/:pageId').get((req, res) => {
+app.route("/page/:pageId").get((req, res) => {
   res.json(pages[req.params.pageId]);
 });
 
 const server = app.listen(4000, () => {
   const port = server.address().port;
 
-  console.log('Server listening at http://localhost:%s', port);
+  console.log("Server listening at http://localhost:%s", port);
 });
